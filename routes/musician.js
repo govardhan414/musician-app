68f2c10260de2ec5eb6e11d1e97090ec98755271
govardhan414 <86237599+govardhan414@users.noreply.github.com>
2021-09-14 15:26:57 +0530

    updated musuician.js

--- routes/musician.js
+++ routes/musician.js
@@ -20,7 +20,7 @@ router.get('/all', (req, res) => {
   });
 });
 
-// retrieve a musician from data store
+ // retrieve a musician from data store
 router.get('/:id', (req, res) => {
   const { musician } = req.app.locals;
   musician.getMusician(req.params.id, (err, returnedMusician) => {
